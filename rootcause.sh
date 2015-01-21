12bb8780d022ee8af655de8fef8b059d032a866c
Judomeades <judomeades@hotmail.com>
2015-01-21 13:31:59 -0500

    Update rootcause.sh

--- rootcause.sh
+++ rootcause.sh
@@ -2,37 +2,38 @@
 # Created by Judomeades and networkant
 # Root cause all in one
 date=`date +'%Y%m%d'`
-#If disk space is greater than 90%
+file=$date"rootcause.log"
+#If disk space is greater than 90% 
 if [[ -n $(df -h | grep 9[0-9]%) ]]; then
-        echo df -h >> $date"rootcause.txt"
+        echo df -h >> $file
 fi
 if [[ -n $(df -h | grep 100%) ]]; then
-        echo df -h >> $date"rootcause.txt"
+        echo df -h >> $file
 fi
 
 #Check for out of memory in /var/log/messages
-grep oom /var/log/messages >> $date"rootcause.txt"
-grep invoked /var/log/messages >> $date"rootcause.txt"
+grep oom /var/log/messages >> $file
+grep invoked /var/log/messages >> $file
 
 #Check to see if there is less than 1GB less in RAM
 Used="$(free -m | grep Mem | awk '{print $4}';)"
 if [ "$Used" -lt "1000" ]; then
-        echo "Available memory is: $Used" >> $date"rootcause.txt"
+        echo "Available memory is: $Used" >> $file
 fi
 
 #look forauthentication failures
-grep "Authentication failed" /var/log/messages >> $date"rootcause.txt"
+grep "Authentication failed" /var/log/messages >> $file
 
 
 #Run status
 if [[ -n $(service mysql status | grep SUCCESS) ]]; then
-        echo "mysql is fine" >> $date"rootcause.txt"
+        echo "mysql is fine" >> $file
 else
-        echo "mysql is either not installed or off" >> $date"rootcause.txt"
+        echo "mysql is either not installed or off" >> $file
 fi
 
 if [[ -n $(service httpd status | grep uptime) ]]; then
-        echo "httpd is fine" >> $date"rootcause.txt"
+        echo "httpd is fine" >> $file
 else
-        echo "Httpd is either not installed or off" >> $date"rootcause.txt"
+        echo "Httpd is either not installed or off" >> $file
 fi
