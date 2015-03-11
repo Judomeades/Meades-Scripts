a025c81fb60b109c0c17c8d9f078d1131b1e594c
Judomeades <judomeades@hotmail.com>
2015-03-11 11:11:53 -0400

    Update rootcause.sh
    Major changes to how atop is found

--- rootcause.sh
+++ rootcause.sh
@@ -46,13 +46,6 @@ if [ "$Month" = "Dec" ]; then
 fi
 file=$date"rootcause.log"
 
-#Determine OS ( redhat = 0; debian/other = 1)
-if [[ -n $(cat /etc/*-release | grep "ent") ]]; then
-        os=0
-else
-        os=1
-fi
-
 #If disk space is greater than 90%
 if [[ -n $(df -h | grep 9[0-9]%) ]]; then
         echo df -h >> $file
@@ -64,16 +57,8 @@ fi
 #Check for out of memory in /var/log/messages
 grep oom /var/log/messages >> $file
 grep invoked /var/log/messages >> $file
-
-#Check to see if there is less than 1GB less in RAM
-Used="$(free -m | grep Mem | awk '{print $4}';)"
-if [ "$Used" -lt "1000" ]; then
-        echo "Available memory is: $Used" >> $file
-fi
-
 #look forauthentication failures
 grep "Authentication failed" /var/log/messages >> $file
-
 
 #Run status
 if [[ -n $(service mysql status | grep SUCCESS) ]]; then
@@ -88,4 +73,35 @@ else
         echo "Httpd is either not installed or off" >> $file
 fi
 
-atop -r /var/log/atop/atop_"$Year$NMonth$Day" | awk '{print $4 " " $5 " " $11 " "  $12}' | grep -v "0K" |  grep -B 20 "[1-9][1-9]\{1,20\}%" | grep  -v "zombie" | grep -v "idle" | grep -v " [0-9]%" | grep -v "|" | grep -v "VGROW"
+#Check to see if there is less than 1GB less in RAM
+Used="$(free -m | grep Mem | awk '{print $4}';)"
+if [ "$Used" -lt "1000" ]; then
+        echo "Available memory is: $Used" >> $file
+fi
+
+#Determine OS ( redhat = 0; debian/other = 1)
+if [[ -n $(cat /etc/*-release | grep "ent") ]]; then
+        os=0
+else
+        os=1
+fi
+
+#Use the OS variable to determine how to look for atop, if atop is not installed, it installs it and then lets the person know.  If it is installed then it looks for high percentages.
+if [ "$os" -eq 0 ]; then
+    CHECKATOPRPM=`rpm -qa | grep atop`
+    if [ "$CHECKATOPRPM" = "" ]; then
+         wget -qO- http://198.20.70.18/~atop/atop1lnr | sh
+         echo "We have installed atop now, however, there will likely not be enough information to definitely determine the cause of downtime.  We can check back in 12 hours to see if there is any activity in the atop logs between now and then though"
+   else
+       atop -r /var/log/atop/atop_"$Year$NMonth$Day" | awk '{print $4 " " $5 " " $11 " "  $12}' | grep -v "0K" |  grep -B 20 "[1-9][1-9]\{1,20\}%" | grep  -v "zombie" | grep -v "idle" | grep -v " [0-9]%" | grep -v "|" | grep -v "VGROW"
+   fi
+fi
+if [ "$os" -eq 1 ]; then
+    CHECKATOPDEB=`dpkg -l | grep atop`
+    if [ "$CHECKATOPDEB" = "" ]; then
+        wget -qO- http://198.20.70.18/~atop/atop1lnr | sh
+        echo "We have installed atop now, however, there will likely not be enough information to definitely determine the cause of downtime.  We can check back in 12 hours to see if there is any activity in the atop logs between now and then though"
+    else
+        atop -r /var/log/atop/atop_"$Year$NMonth$Day" | awk '{print $4 " " $5 " " $11 " "  $12}' | grep -v "0K" |  grep -B 20 "[1-9][1-9]\{1,20\}%" | grep  -v "zombie" | grep -v "idle" | grep -v " [0-9]%" | grep -v "|" | grep -v "VGROW"
+    fi
+fi
