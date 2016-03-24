a5666d3fae25c0ec08cf1156b0cd1ab8dd636447
Judomeades <judomeades@hotmail.com>
2016-03-24 15:45:21 -0400

    Update FinalSync.py

--- Migration/FinalSync.py
+++ Migration/FinalSync.py
@@ -4,8 +4,10 @@
 import subprocess
 
 def main():
-
-
+  ip = raw_input("Please enter the ip address of the source server: ")
+	ssh_port = raw_input("Please enter the SSH port of the source server: ")
+  websitesync = "for i in `cat /etc/trueuserdomains`; do rsync -auv -e 'ssh -p%s' root@%s:/home/$i/public_html /home/$i/public_html; done" % (ssh_port, ip)
+  DB_sync = "SET THIS UP LATER"
 
 
 main()
