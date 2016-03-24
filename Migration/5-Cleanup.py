b6a25a3acc13ce43608b438821ef56fc64f73884
Judomeades <judomeades@hotmail.com>
2016-03-24 08:30:31 -0400

    Update and rename 5-Cleanupcpmoves.py to 5-Cleanup.py

--- Migration/5-Cleanup.py
+++ Migration/5-Cleanup.py
@@ -4,5 +4,8 @@
 import subprocess
 def main():
 	remove_cpmoves = "rm -rf /home/cpmovemigration"
+	remove_scripts = "rm -f 0-CreateKey.py; rm -f 1-QA.py; rm -f 2-EnvironmentMatch.py; rm -f 3-PackageAccounts.py; rm -f 4-SyncPull.py; rm -f 5-Cleanup.py"
 	subprocess.call([remove_cpmoves], shell=True)
+	subprocess.call([remove_scripts], shell=True)
+	
 main()
