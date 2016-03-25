4913a3074580ed552b43db247b93071b57ae2d2f
Judomeades <judomeades@hotmail.com>
2016-03-25 09:52:35 -0400

    Update 6-IPSwap.py

--- Migration/6-IPSwap.py
+++ Migration/6-IPSwap.py
@@ -5,9 +5,12 @@
 #This is still pretty early stages of development
 import subprocess
 def main():
+	ip = raw_input("Please enter the ip address of the source server: ")
+	ssh_port = raw_input("Please enter the SSH port of the source server: ")
 	create_backups = "cp /etc/hosts /etc/hosts.dst && cp /etc/ips /etc/ips.dst && cp /etc/sysconfig/network /etc/sysconfig/network.dst && cp /var/cpanel/mainip /var/cpanel/mainip.dst"
 	swap_values = "echo 'Placeholder variable, nothing has been changed yet'"
-	Activate
+	update_values = "/usr/local/cpanel/cpkeyclt; service ipaliases reload; service named reload; /scripts/rebuildippool; /scripts/rebuildhttpdconf"
 	subprocess.call([create_backups], shell=True)
 	subprocess.call([swap_values], shell=True)
+	subprocess.call([update_values], shell=True)
 main()
