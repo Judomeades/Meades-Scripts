0cc6cc50ace7730fa994cb6df1542ac4e14bc495
Judomeades <judomeades@hotmail.com>
2016-03-25 09:53:27 -0400

    Update 2-EnvironmentMatch.py

--- Migration/2-EnvironmentMatch.py
+++ Migration/2-EnvironmentMatch.py
@@ -7,10 +7,10 @@ def main():
 	ip = raw_input("Please enter the ip address of the source server: ")
 	ssh_port = raw_input("Please enter the SSH port of the source server: ")
 	#Get config files
-	pullmysqlconf = "rsync -auvI -e 'ssh -p%s' root@%s:/etc/my.cnf /etc/" % (ssh_port, ip)
-	pullphpini = "rsync -auvI -e 'ssh -p%s' root@%s:/usr/local/lib/php.ini /usr/local/lib/" % (ssh_port, ip)
-	pulleabuild = "rsync -auvI -e 'ssh -p%s' root@%s:/var/cpanel/easy/apache/profile/_last_success.yaml  /var/cpanel/easy/apache/profile/" % (ssh_port, ip)
-	pullcrontab = "rsync -auvI -e 'ssh -p%s' root@%s:/etc/crontab /etc/" % (ssh_port, ip)
+	pullmysqlconf = "rsync -avI -e 'ssh -p%s' root@%s:/etc/my.cnf /etc/" % (ssh_port, ip)
+	pullphpini = "rsync -avI -e 'ssh -p%s' root@%s:/usr/local/lib/php.ini /usr/local/lib/" % (ssh_port, ip)
+	pulleabuild = "rsync -avI -e 'ssh -p%s' root@%s:/var/cpanel/easy/apache/profile/_last_success.yaml  /var/cpanel/easy/apache/profile/" % (ssh_port, ip)
+	pullcrontab = "rsync -avI -e 'ssh -p%s' root@%s:/etc/crontab /etc/" % (ssh_port, ip)
 	
 	runeasyapache = "/scripts/easyapache"
 	
