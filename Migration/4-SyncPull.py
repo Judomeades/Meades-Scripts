e6df93ecb14863983bbf0ceb08d23ae8fafd2ab7
Judomeades <judomeades@hotmail.com>
2016-07-01 00:33:50 -0500

    Update 4-SyncPull.py

--- Migration/4-SyncPull.py
+++ Migration/4-SyncPull.py
@@ -8,7 +8,7 @@ def main():
 	ssh_port = raw_input("Please enter the SSH port of the source server: ")
 	rynsc_pull = "rsync -auv -e 'ssh -p%s' root@%s:/home/cpmovemigration /home/" % (ssh_port, ip)
 	restore_cpmoves = "for i in `ls /home/cpmovemigration`; do /scripts/restorepkg /home/cpmovemigration/$i; done"
-	sync_home_dirs = "for i in `cat /etc/domainusers | cut -d: -f1`; rsync -auv -e 'ssh -p%s' root@%s:/home/$i /home/" % (ssh_port, ip)
+	sync_home_dirs = "for i in `cat /etc/domainusers | cut -d: -f1`; rsync -auv -e 'ssh -p%s' root@%s:/home/$i /home/; done" % (ssh_port, ip)
 	
 	subprocess.call([rynsc_pull], shell=True)
 	subprocess.call([restore_cpmoves], shell=True)
